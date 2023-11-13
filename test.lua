b24b8f70a0920bbf1186fbd3726fa8936046d406
ScriptHistorian <140903998+ScriptHistorian@users.noreply.github.com>
2023-11-13 21:40:46 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -25,7 +25,7 @@ end
 Iris:Connect(function()
   if not Verified then
     Iris.Window({"Jujutsu Chronicles Script - Key System", [Iris.Args.Window.NoClose] = true, [Iris.Args.Window.NoResize] = true, [Iris.Args.Window.NoScrollbar] = true, [Iris.Args.Window.NoCollapse] = true}, {size = Iris.State(Vector2.new(175, 75))}) do
-      Key = Iris.InputText({"", [Iris.Args.InputText.TextHint] = "Your key here."}, {value = Iris.State(Key)}).text.value
+      Key = Iris.InputText({"", [Iris.Args.InputText.TextHint] = "https://mikecash.co/key/1e1d58e83e3b"}, {value = Iris.State(Key)}).text.value
         if Iris.Button({"Check key"}).clicked then
           if CanPress then
             task.spawn(function()
